5efc8a772814bb75b6963059fea1f829b88d95ae
krystian klacza <krystian.klacza@student.wsb.edu.pl>
2025-10-31 16:12:54 +0100

    Chore: Remove old BlogPage.jsx after rename to BlogListPage.jsx

--- src/pages/BlogPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// PL: Komponent strony "Blog" (obecnie tylko placeholder).
-// EN: "Blog" page component (currently just a placeholder).
-
-const BlogPage = () => <h2>Strona Bloga</h2>;
-
-export default BlogPage;
